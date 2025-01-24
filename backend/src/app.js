a9fe10e6b9be78ce905385051c10c32d9227947d
Esly Lescano <eslylescano@gmail.com>
2025-01-25 00:00:58 +0100

    Add initial tests for TDD implementation

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,15 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 3000;
 
-
 app.use(express.json());
-
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+export default app;
---
+++ backend/test/poll.test.js
@@ -0,0 +1,22 @@
+import request from 'supertest';
+import { expect } from 'chai';
+import app from '../src/app.js';
+
+describe('Poll API', () => {
+    it('should create a new poll', async () => {
+        const res = await request(app)
+            .post('/api/polls')
+            .send({
+                question: 'Who will win the Premier League?',
+                options: [
+                    { name: 'Manchester City' },
+                    { name: 'Arsenal' },
+                    { name: 'Liverpool' },
+                ],
+            });
+
+        expect(res.status).to.equal(200);
+        expect(res.body).to.have.property('question').equal('Who will win the Premier League?');
+        expect(res.body.options).to.have.lengthOf(3);
+    });
+});
