d871190e651c26cf4f9c13b7f00bb63f144912ee
Esly Lescano <eslylescano@gmail.com>
2025-01-26 21:33:50 +0100

    Add a default route to test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { PollProvider } from './context/PoolContext';
 import VotePage from './pages/VotePage/VotePage';
 import ResultsPage from './pages/ResultsPage/ResultsPage';
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/vote/6796913d2086c1718b444661" replace />} />
         <Route
           path="/vote/:pollId"
           element={
