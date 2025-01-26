a4b38f59c165319b0e371539b55f7b2c53319b4d
Esly Lescano <eslylescano@gmail.com>
2025-01-26 17:20:52 +0100

    use better mock data

--- frontend/src/pages/VotePage/VotePage.js
+++ frontend/src/pages/VotePage/VotePage.js
@@ -6,16 +6,25 @@ import SubmitButton from '../../components/SubmitButton/SubmitButton';
 import './VotePage.css';
 
 function VotePage() {
-    const poll = {
+    const pollData = {
+        _id: '6794e3a549e1cdbcc188254e',
         question: 'Who will win the Premier League?',
-        options: ['Manchester City', 'Arsenal', 'Liverpool'],
+        options: [
+            { name: 'Manchester City', votes: 3, _id: '6794e3a549e1cdbcc188254f' },
+            { name: 'Arsenal', votes: 2, _id: '6794e3a549e1cdbcc1882550' },
+            { name: 'Liverpool', votes: 0, _id: '6794e3a549e1cdbcc1882551' },
+        ],
+        createdAt: '2025-01-25T13:14:13.317Z',
+        __v: 0,
     };
+
+    const options = pollData.options.map(option => option.name);
 
     return (
         <div className="container">
             <Logo />
-            <PollQuestion question={poll.question} />
-            <PollOptions options={poll.options} />
+            <PollQuestion question={pollData.question} />
+            <PollOptions options={options} />
             <SubmitButton />
         </div>
     );
